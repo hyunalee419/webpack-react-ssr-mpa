cf15c22db61a8c062273cc52cfc5a91f78c3ae89
hyunalee <dlgusdk419@naver.com>
2019-05-16 17:59:38 +0900

    refactor(about): change hook

--- client/pages/about/About.js
+++ client/pages/about/About.js
@@ -1,29 +1,24 @@
-import React, {Component} from 'react';
+import React, { useEffect, useState } from 'react';
 
-class About extends Component {
+const About = ({
+	data
+}) => {
+	const [ text, setText ] = useState('About Server-Side Rendering!!!!');
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			text: 'About Server-Side Rendering!!!!'
-		}
-	}
+	const firstUpdate = React.useRef(true);
+	useEffect(() => {
+		if ( !firstUpdate.current ) return;
 
-	componentDidMount(){
-		this.setState({
-			text : 'About Client Loaded'
-		})
-	}
+		firstUpdate.current = false;
+		setText('About Client Loaded');
+	});
 
-	render() {
-		const { data } = this.props;
-		return (
-			<div className="hello">
-				<p>{data.text}</p>
-				<h1>{this.state.text} Hello</h1>
-			</div>
-		);
-	}
-}
+	return (
+		<div className="hello">
+			<p>{data.text}</p>
+			<h1>{text} Hello</h1>
+		</div>
+	);
+};
 
 export default About;
