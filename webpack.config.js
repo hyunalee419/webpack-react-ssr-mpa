648c7165775a058ebb9cf0121928236ca738399c
hyunalee <dlgusdk419@naver.com>
2019-05-16 17:49:31 +0900

    refactor(webpack): refactor, add fetch polyfill

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,46 @@
- const { resolve } = require('path');
- const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { resolve } = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
- const PATH_SOURCE = resolve(__dirname, 'client');
+const PATH_SOURCE_PAGE = resolve(__dirname, 'client/pages');
+const PATH_SOURCE_SCSS = resolve(__dirname, 'client/scss');
 
- module.exports = {
-	 entry: {
-		 index: ['@babel/polyfill', PATH_SOURCE + '/pages/index/index.js', PATH_SOURCE + '/scss/index/index.scss'],
-		 about: ['@babel/polyfill', PATH_SOURCE + '/pages/about/index.js', PATH_SOURCE + '/scss/about/index.scss'],
-	 },
-	 output : {
-		 filename : '[name]/client.bundle.js', // output filename
-			 path : resolve(__dirname, 'build'), // output path
-			 publicPath : '/'
-	 },
+const COMMON_ENTRY = ['@babel/polyfill', 'whatwg-fetch'];
+module.exports = {
+  entry: {
+		index: [...COMMON_ENTRY, PATH_SOURCE_PAGE + '/index/index.js', PATH_SOURCE_SCSS + '/index/index.scss'],
+		about: [...COMMON_ENTRY, PATH_SOURCE_PAGE + '/about/index.js', PATH_SOURCE_SCSS + '/about/index.scss'],
+	},
+	output : {
+		filename : '[name]/client.bundle.js', // output filename
+		path : resolve(__dirname, 'build'), // output path
+		publicPath : '/'
+	},
 
-	 module : {
-		 rules : [
-			 {
-				 test : /\.jsx?$/,
-				 exclude : /node_modules/,
-				 use : {
-					 loader : 'babel-loader',
-					 options : {
-						 presets : ['@babel/preset-env', '@babel/preset-react']
-					 },
-				 }
-			 }, {
-				 test: /\.s?css$/,
-				 use: [
-					 MiniCssExtractPlugin.loader,
-					 'css-loader',
-					 'sass-loader'
-				 ]
-			 }
-		 ],
-	 },
-	 resolve: {
-		 extensions: ['.css', '.scss', '.js', '.jsx']
-	 },
-	 plugins: [
-		 // 기타 플러그인
-		 new MiniCssExtractPlugin({ filename: '[name]/[name].css' })
-	 ]
- };
+	module : {
+		rules : [{
+			test : /\.jsx?$/,
+			exclude : /node_modules/,
+			use : {
+				loader : 'babel-loader',
+				options : {
+					presets : ['@babel/preset-env', '@babel/preset-react']
+				},
+			}
+		}, {
+			test: /\.s?css$/,
+			use: [
+				MiniCssExtractPlugin.loader,
+				'css-loader',
+				'sass-loader'
+			]
+		}
+		],
+	},
+	resolve: {
+  	extensions: ['.css', '.scss', '.js', '.jsx']
+	},
+	plugins: [
+		// 기타 플러그인
+		new MiniCssExtractPlugin({ filename: '[name]/[name].css' })
+	]
+};
